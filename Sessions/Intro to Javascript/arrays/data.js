674ded35ef0c7d412c4c6516f96f998e7773b9da
fjollespahiu <fjolle.spahiu@gmail.com>
2025-01-22 14:21:05 +0100

    up

---
+++ Sessions/Intro to Javascript/arrays/data.js
@@ -0,0 +1,10 @@
+let allcars = [
+    ['#', 'Brand', 'Model', 'Year'],
+    {
+        cars: [
+            { brand: "BMW", model: "740i", year: "2020" },
+            { brand: "Rolls Royce", model: "Phantom", year: "2024" },
+            { brand: "Mercedes", model: "CLS63", year: "2014" }
+        ]
+    }
+];
